36fe4391b49c2e5253ea286f1c513f9f688ac649
Sanmu3 <mujahidmuslim0@gmail.com>
2020-09-10 22:57:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var reactNative = ['Hafif' , 'Dayat', 'Wandi']
+var reactJS = ['yusuf', 'luthfi', 'tompel'] 
+
+var JavaScriptDivision = reactNative.concat(reactJS)
+
+console.log(JavaScriptDivision)
